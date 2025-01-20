23334ec7ea52c608f729af24986f026a41adbcbc
Chronos000324 <quantumit.lina@gmail.com>
2025-01-20 08:11:22 +0800

    Update scene1-en.js

--- page/js/scene1-en.js
+++ page/js/scene1-en.js
@@ -1,5 +1,5 @@
-//import { loadGLTF, loadAudio } from "../../libs/loader.js";
-import { loadGLTF, loadAudio } from "https://cdn.jsdelivr.net/gh/Chronos000324/artest2/libs/loader.js";
+import { loadGLTF, loadAudio } from "../../libs/loader.js";
+//import { loadGLTF, loadAudio } from "https://cdn.jsdelivr.net/gh/Chronos000324/artest2/libs/loader.js";
 
 const THREE = window.MINDAR.IMAGE.THREE;
 
